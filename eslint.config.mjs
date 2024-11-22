febe1cedaabec72e53b77b3fa0d0e198765bf874
awinardi1004 <awinardi1004@gmail.com>
2024-11-22 09:42:12 +0700

    updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,9 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import daStyle from 'eslint-config-dicodingacademy';
-
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  daStyle,
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
