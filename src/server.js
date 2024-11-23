4969bfa1710986b975e793e402b1a3cc02d5fece
awinardi1004 <awinardi1004@gmail.com>
2024-11-23 11:12:03 +0700

    updated

--- src/server.js
+++ src/server.js
@@ -3,21 +3,21 @@ const routes = require('./routes');
 
 
 const init = async () => {
-    const server = Hapi.server({
-        port: 5000,
-        host: 'localhost',
-        routes: {
-            cors: {
-                origin: ['http://notesapp-v1.dicodingacademy.com'],
-            },
-        },
-     });
-     
-     server.route(routes);
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['http://notesapp-v1.dicodingacademy.com'],
+      },
+    },
+  });
+
+  server.route(routes);
 
 
-    await server.start();
-    console.log(`Server berjalan pada ${server.info.uri}`);
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
 };
- 
+
 init();
